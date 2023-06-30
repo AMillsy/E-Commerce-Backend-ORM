7732d067d8d8c61698d05f16e92762b00507b404
AMillsy <00adammills00@gmail.com>
2023-06-30 16:21:41 +0100

    setting up all the models and relationships

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,19 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define column
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
--- models/index.js
+++ models/index.js
@@ -1,17 +1,25 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "categoryId",
 
+  onDelete: "CASCADE",
+});
 // Categories have many Products
-
+Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: "Product_Tag",
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: "Product_Tag",
+});
 module.exports = {
   Product,
   Category,
