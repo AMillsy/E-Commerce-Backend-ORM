c1841987be0f62ae9253cadade0080f9d93475cf
AMillsy <00adammills00@gmail.com>
2023-06-30 19:37:24 +0100

    can delete tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -69,8 +69,14 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagDeleted = await Tag.destroy({ where: { id: req.params.id } });
+    res.status(200).json(tagDeleted);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
